25f00ae67686e596db0d597c7f2ac85996e29beb
Piyush <piyush.kalyani7@gmail.com>
2023-12-30 14:57:19 +0530

    homescreen complete

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,20 @@
-// pages/Home.js
 import React, { useEffect, useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Card1 from '../components/Card';
 import axios from 'axios';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 const Home = () => {
-  const [Data, setData] = useState([]);
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 12;
+  // const totalPages = Math.ceil(data.length / itemsPerPage);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://shikimori.one/api/animes?page=1&&limit=12');
+        const response = await axios.get(`https://shikimori.one/api/animes?page=${currentPage}&&limit=${itemsPerPage}`);
         setData(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -18,16 +22,30 @@ const Home = () => {
     };
 
     fetchData();
-  }, []);
+  }, [currentPage]);
+
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
+  };
 
   return (
-    <Grid container spacing={3}>
-      {Data.map(anime => (
-        <Grid item key={anime.id} xs={12} sm={6} md={4} lg={3}>
-          <Card1 anime={anime} />
-        </Grid>
-      ))}
-    </Grid>
+    <>
+      <Grid container spacing={3}>
+        {data.map((anime) => (
+          <Grid item key={anime.id} xs={12} sm={6} md={4} lg={3}>
+            <Card1 anime={anime} />
+          </Grid>
+        ))}
+      </Grid>
+      <Stack spacing={2} sx={{ marginTop: 2 }}>
+        <Pagination
+          count= {500}
+          page={currentPage}
+          color="primary"
+          onChange={handlePageChange}
+        />
+      </Stack>
+    </>
   );
 };
 
