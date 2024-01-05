049ba1a2376a32223cc0ad0024f61162898f0292
Piyush <piyush.kalyani7@gmail.com>
2024-01-05 21:52:38 +0530

    loading in google login

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,14 +1,18 @@
+import React from 'react';
 import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
 import { app } from '../firebase';
 import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/userSlice';
 import { useNavigate } from 'react-router-dom';
+import CircularProgress from '@mui/material/CircularProgress'; // Import CircularProgress
 
 export default function OAuth() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = React.useState(false); // New state for loading
   const handleGoogleClick = async () => {
     try {
+      setIsLoading(true)
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
 
@@ -25,6 +29,7 @@ export default function OAuth() {
         }),
       });
       const data = await res.json();
+      setIsLoading(false)
       console.log(data);
       dispatch(signInSuccess(data));
       navigate('/');
@@ -33,12 +38,20 @@ export default function OAuth() {
     }
   };
   return (
-    <button
-      type='button'
-      onClick={handleGoogleClick}
-      className='bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95'
-    >
-      Continue with google
-    </button>
+    <>
+      {isLoading ? (
+        <div className=" items-center h-screen">
+          <CircularProgress />
+        </div>
+      ) : (
+        <button
+          type='button'
+          onClick={handleGoogleClick}
+          className='bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95'
+        >
+          Continue with google
+        </button>)}
+
+    </>
   );
 }
